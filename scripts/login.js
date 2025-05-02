71bf182859ed9ce0ced148fd8f51a93f956ec526
James Klick <jklick@ivytech.edu>
2025-05-02 19:52:15 -0400

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -14,7 +14,7 @@ async function login(username,password){
         password
     }
     // send login POST request to backend
-    const response = await fetch("https://rustic-festive-shirt.glitch.me/api/auth/",{
+    const response = await fetch("https://rustic-festive-shirt.glitch.me/api/auth",{
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
